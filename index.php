f156f6384dd3f2f6d683f152f7c43ddaa7357472
bmsmx <mvg@bms.mx>
2020-05-16 09:08:27 -0500

    2020-05-16 Carga inicial del index

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: EYSD Formulario
+ * Author: Manuel Villavicencio G.
+ * Description: Formulario personalizado que utiliza shortcode [eysd-form1]
+ * Version: 1.0
+ */
+?>
