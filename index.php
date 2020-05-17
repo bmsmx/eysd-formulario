b254666eefa19b6b023ff8b190d20c6321c38e98
bmsmx <mvg@bms.mx>
2020-05-17 08:23:51 -0500

    Actualización

--- index.php
+++ index.php
@@ -18,11 +18,51 @@ class="cuestionario"">
             <label for="nombre">Nombre</label>
             <input type="text" name="nombre" id="nombre" required>
         </div>
-        
+        <div class="form-input">
+            <label for='correo'>Correo</label>
+            <input type="email" name="correo" id="correo" required>
+        </div>
+        <div class="form-input">
+            <label for="nivel_html">¿Cuál es tu nivel de HTML?</label>
+            <input type="radio" name="nivel_html" value="1" required> Nada
+            <br><input type="radio" name="nivel_html" value="2" required> Estoy 
+                aprendiendo
+            <br><input type="radio" name="nivel_html" value="3" required> Tengo 
+                experiencia
+            <br><input type="radio" name="nivel_html" value="4" required> Lo 
+                domino al dedillo
+        </div>
+        <div class="form-input">
+            <label for="nivel_css">¿Cuál es tu nivel de CSS?</label>
+            <input type="radio" name="nivel_css" value="1" required> Nada
+            <br><input type="radio" name="nivel_css" value="2" required> Estoy 
+                aprendiendo
+            <br><input type="radio" name="nivel_css" value="3" required> Tengo 
+                experiencia
+            <br><input type="radio" name="nivel_css" value="4" required> Lo 
+                domino al dedillo
+        </div>
+        <div class="form-input">
+            <label for="nivel_js">¿Cuál es tu nivel de JavaScript?</label>
+            <input type="radio" name="nivel_js" value="1" required> Nada
+            <br><input type="radio" name="nivel_js" value="2" required> Estoy 
+                aprendiendo
+            <br><input type="radio" name="nivel_js" value="3" required> Tengo 
+                experiencia
+            <br><input type="radio" name="nivel_js" value="4" required> Lo domino 
+al dedillo
+        </div>
+        <div class="form-input">
+            <label for="aceptacion">La información facilitada se tratará 
+            con respeto y admiración.</label>
+            <input type="checkbox" id="aceptacion" name="aceptacion"
+value="1" required> Entiendo y acepto las condiciones
+        </div>
         <div class="form-input">
             <input type="submit" value="Enviar">
         </div>
     </form>
+    
 
 <?php
 return ob_get_clean();
