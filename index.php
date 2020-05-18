84704fc5a29095e89a9ddb32725081b0a6eb8b01
bmsmx <mvg@bms.mx>
2020-05-18 12:21:12 -0500

    Rev

--- index.php
+++ index.php
@@ -9,61 +9,59 @@
 add_shortcode('eysd_form1','eysd_form1');
 function eysd_form1()
 {
-    wp_enqueue_style('css_aspirante', plugins_url('style.css', __FILE__));
+    wp_enqueue_style('css_aspirante', plugins_url('/eysd-formulario/style.css', __FILE__));
     ob_start();
 
 ?>
 
-<form action="<?php get_the_permalink(); ?>" method="post" id="form1"
-class="cuestionario">
-        <div class="form-input">
-            <label for="nombre">Nombre</label>
-            <input type="text" name="nombre" id="nombre" required>
-        </div>
-        <div class="form-input">
-            <label for='correo'>Correo</label>
-            <input type="email" name="correo" id="correo" required>
-        </div>
-        <div class="form-input">
-            <label for="nivel_html">¿Cuál es tu nivel de HTML?</label>
-            <input type="radio" name="nivel_html" value="1" required> Nada
-            <br><input type="radio" name="nivel_html" value="2" required> Estoy 
-                aprendiendo
-            <br><input type="radio" name="nivel_html" value="3" required> Tengo 
-                experiencia
-            <br><input type="radio" name="nivel_html" value="4" required> Lo 
-                domino al dedillo
-        </div>
-        <div class="form-input">
-            <label for="nivel_css">¿Cuál es tu nivel de CSS?</label>
-            <input type="radio" name="nivel_css" value="1" required> Nada
-            <br><input type="radio" name="nivel_css" value="2" required> Estoy 
-                aprendiendo
-            <br><input type="radio" name="nivel_css" value="3" required> Tengo 
-                experiencia
-            <br><input type="radio" name="nivel_css" value="4" required> Lo 
-                domino al dedillo
-        </div>
-        <div class="form-input">
-            <label for="nivel_js">¿Cuál es tu nivel de JavaScript?</label>
-            <input type="radio" name="nivel_js" value="1" required> Nada
-            <br><input type="radio" name="nivel_js" value="2" required> Estoy 
-                aprendiendo
-            <br><input type="radio" name="nivel_js" value="3" required> Tengo 
-                experiencia
-            <br><input type="radio" name="nivel_js" value="4" required> Lo domino 
-al dedillo
-        </div>
-        <div class="form-input">
-            <label for="aceptacion">La información facilitada se tratará 
+<form action="<?php get_the_permalink(); ?>" method="post" id="form1" class="cuestionario">
+    <div class="form-input">
+        <label for="nombre">Nombre</label>
+        <input type="text" name="nombre" id="nombre" required>
+    </div>
+    <div class="form-input">
+        <label for='correo'>Correo</label>
+        <input type="email" name="correo" id="correo" required>
+    </div>
+    <div class="form-input">
+        <label for="nivel_html">¿Cuál es tu nivel de HTML?</label>
+        <input type="radio" name="nivel_html" value="1" required> Nada
+        <br><input type="radio" name="nivel_html" value="2" required> Estoy
+        aprendiendo
+        <br><input type="radio" name="nivel_html" value="3" required> Tengo
+        experiencia
+        <br><input type="radio" name="nivel_html" value="4" required> Lo
+        domino al dedillo
+    </div>
+    <div class="form-input">
+        <label for="nivel_css">¿Cuál es tu nivel de CSS?</label>
+        <input type="radio" name="nivel_css" value="1" required> Nada
+        <br><input type="radio" name="nivel_css" value="2" required> Estoy
+        aprendiendo
+        <br><input type="radio" name="nivel_css" value="3" required> Tengo
+        experiencia
+        <br><input type="radio" name="nivel_css" value="4" required> Lo
+        domino al dedillo
+    </div>
+    <div class="form-input">
+        <label for="nivel_js">¿Cuál es tu nivel de JavaScript?</label>
+        <input type="radio" name="nivel_js" value="1" required> Nada
+        <br><input type="radio" name="nivel_js" value="2" required> Estoy
+        aprendiendo
+        <br><input type="radio" name="nivel_js" value="3" required> Tengo
+        experiencia
+        <br><input type="radio" name="nivel_js" value="4" required> Lo domino
+        al dedillo
+    </div>
+    <div class="form-input">
+        <label for="aceptacion">La información facilitada se tratará
             con respeto y admiración.</label>
-            <input type="checkbox" id="aceptacion" name="aceptacion"
-value="1" required> Entiendo y acepto las condiciones
-        </div>
-        <div class="form-input">
-            <input type="submit" value="Enviar">
-        </div>
-    </form>
+        <input type="checkbox" id="aceptacion" name="aceptacion" value="1" required> Entiendo y acepto las condiciones
+    </div>
+    <div class="form-input">
+        <input type="submit" value="Enviar">
+    </div>
+</form>
 
 
 <?php
